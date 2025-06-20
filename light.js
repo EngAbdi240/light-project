f1e4009467215362ecd520698daa6e6b6b715aa5
abdi <engabdi85@gmail.com>
2025-06-20 14:33:42 -0700

    Light project

---
+++ light.js
@@ -0,0 +1,14 @@
+
+
+
+    const img1 = document.querySelector("#img1");
+    const img2 = document.querySelector("#img2");
+    const button1 = document.querySelector("#btn1");
+    const button2 = document.querySelector("#btn2");
+
+    function hideImage() {
+      img1.style.display = "none"; 
+    }
+     function Displaymage() {
+      img1.style.display = "block"; 
+    }
